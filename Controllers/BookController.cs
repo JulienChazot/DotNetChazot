ffb600096b079123821f6fa6b302e9e386b2b02b
JulienChazot <Julien.chazot@lesbonstech.com>
2023-12-05 16:28:13 +0100

    Premier Post + Afficher par ID

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using newWebAPI.Models;
 
 namespace newWebAPI.Controllers;
@@ -10,9 +11,9 @@ public class BookController: ControllerBase
 {
     private readonly AppDbContext _context;
 
-    BookController(AppDbContext context)
+    public BookController(AppDbContext context)
     {
-        this._context = context;
+        _context = context;
     }
 
     [HttpGet]
@@ -21,4 +22,49 @@ public class BookController: ControllerBase
     {
             return _context.Books.ToList();
     }
+
+    [HttpGet("{id}", Name = nameof(GetBook))]
+
+    public async Task<ActionResult<Book>> GetBook (int id)
+    {
+        var book = await _context.Books.FindAsync(id);
+        if (book == null)
+        {
+            return NotFound();
+        }
+        
+        return book;
+    }
+
+    [HttpPost]
+    [ProducesResponseType(201, Type = typeof(Book))]
+    [ProducesResponseType(400)]
+
+    public async Task<ActionResult<Book>> PostBook([FromBody] Book book)
+    {
+        if (book == null)
+        {
+            return BadRequest("L'objet book est nul dans le post");
+        }
+        Book? addedBook = await _context.Books.FirstOrDefaultAsync(b => b.Title == book.Title);
+        if (addedBook != null)
+        {
+            return BadRequest("L'objet book existe déjà");
+        }
+        else {
+            await _context.Books.AddAsync(book);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtRoute(
+                routeName: nameof(GetBook),
+                routeValues: new { id = book.Id},
+                value: book);
+            
+        }
+    }
+
+    // TODO : PUT : api/book/[id] creer la route qui permet de mettre a jour un livre existant
+    // TODO: DELETE : api/book/[id] supprimer un livre
+
+
 }
--- Models/AppDbContext.cs
+++ Models/AppDbContext.cs
@@ -18,7 +18,71 @@ public class AppDbContext : DbContext
          optionsBuilder.UseSqlite($"Data Source={dbPath}");
      }
 
-    //On va créer une propriété pour chaque table de la BDD
-    public DbSet<Book> Books {get; set;}
-    public DbSet<Empreint> Empreints {get; set;}
+     
+
+     protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Book>().HasData(
+            new Book
+            {
+                Id = 1,
+                Title = "Professional C# 6 and .NET Core 1.0",
+                Autor = "Christian Nagel",
+                Description = "A true masterclass in C# and .NET programming",
+                Genre = "Software",
+                Price = 50,
+                PublishDate = new DateTime(2016, 05, 09)
+            },
+            new Book
+            {
+                Id = 2,
+                Title = "Professional C# 7 and .NET Core 2.0",
+                Autor = "Christian Nagel",
+                Description = "A true masterclass in C# and .NET programming",
+                Genre = "Software",
+                Price = 50,
+                PublishDate = new DateTime(2018, 04, 18)
+            },
+            new Book
+            {
+                Id = 3,
+                Title = "Professional C# 8 and .NET Core 3.0",
+                Autor = "Christian Nagel",
+                Description = "A true masterclass in C# and .NET programming",
+                Genre = "Software",
+                Price = 50,
+                PublishDate = new DateTime(2019, 10, 30)
+            },
+            new Book
+            {
+                Id = 4,
+                Title = "Professional C# 9 and .NET 5",
+                Autor = "Christian Nagel",
+                Description = "A true masterclass in C# and .NET programming",
+                Genre = "Software",
+                Price = 50,
+                PublishDate = new DateTime(2021, 01, 01)
+            },
+            new Book
+            {
+                Id = 5,
+                Title = "Beginning Visual C# 2019",
+                Autor = "Perkins, Reid, and Hammer",
+                Description = "The perfect guide to Visual C#",
+                Genre = "Software",
+                Price = 45,
+                PublishDate = new DateTime(2020, 09, 23)
+            },
+            new Book
+            {
+                Id = 6,
+                Title = "Pro C# 7",
+                Autor = "Andrew Troelsen",
+                Description = "The ultimate C# resource",
+                Genre = "Software",
+                Price = 50,
+                PublishDate = new DateTime(2017, 10, 01)
+            });
+    }
+    public DbSet<Book> Books { get; set; } = default!;
 }
